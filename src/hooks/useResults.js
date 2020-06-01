593f3c52728d0c3b25dfb9e2b2aa736bd58c64de
erica2605@live.com <erica2605@live.com>
2020-06-01 14:26:11 -0400

    🌟 [updated] search city and term for maximum test results

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -11,7 +11,7 @@ export default () => {
         params: {
           limit: 50,
           term: searchTerm,
-          location: 'san jose'
+          location: 'san francisco'
         }
       });
       setResults(response.data.businesses);
@@ -21,7 +21,7 @@ export default () => {
   }
 
   useEffect(() => {
-    searchApi('pasta');
+    searchApi('cocktail');
   }, []);
 
   return [searchApi, results, errorMessage]
